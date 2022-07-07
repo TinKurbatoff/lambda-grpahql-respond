18cbbd851fecd8c817c7cebe035bbc8169ef3615
Constantine Kurbatoff <6782841@gmail.com>
2022-07-06 23:42:22 -0400

    extracted graphql.schema to a separate file

--- request_example.sh
+++ request_example.sh
@@ -2,4 +2,4 @@
 AWS_URL=$1
 curl -X POST https://$AWS_URL/default/graphql-new-test \
 			-H "content-type: application/json" \
-			--data-raw "{\"query\":\"query (\$address: String, \$city: String) {\n  addrListItems (primary_lane: \$address, city: \$city) {\n    status\n    message\n    itemsCount\n    data {\n      primary_lane\n      city\n      state\n      latitude\n      longitude\n    }\n  }\n}\n \",\"variables\":{\"city\":\"OAKLAND\",\"address\":\"333 WAYNE AVE APT E\"}}" 
+			--data-raw "{\"query\":\"query (\$address: String, \$city: String) {\n  addrListItems (primary_lane: \$address, city: \$city) {\n    status\n    message\n    itemsCount\n    data {\n formatted_street_address\n      city\n      state\n      latitude\n      longitude\n    }\n  }\n}\n \",\"variables\":{\"city\":\"OAKLAND\",\"address\":\"777 W\"}}" 
